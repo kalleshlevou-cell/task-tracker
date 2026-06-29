cc6b992dee02d242f48dffbf3e641426e8a6ad0b
Kallesh <kalleshlevou@gmail.com>
2026-06-29 16:25:31 +0530

    ok

--- task-tracker/client/vite.config.js
+++ task-tracker/client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.GITHUB_ACTIONS ? '/task-tracker/' : '/',
   server: {
     port: 3000,
     proxy: {
